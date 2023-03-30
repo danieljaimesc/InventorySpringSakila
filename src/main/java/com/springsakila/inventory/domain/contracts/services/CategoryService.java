1bd8b11d934d6eee0db42711548aef75ed069b77
Daniel Jaimes Carrillo <daniel.jaimes-carrillo@capgemini.com>
2023-03-30 12:04:34 +0200

    contracts services

---
+++ src/main/java/com/springsakila/inventory/domain/contracts/services/CategoryService.java
@@ -0,0 +1,8 @@
+package com.springsakila.inventory.domain.contracts.services;
+
+
+import com.springsakila.inventory.domain.entities.Category;
+
+public interface CategoryService extends ProjectionDomainService<Category, Integer> {
+
+}
---
+++ src/main/java/com/springsakila/inventory/domain/contracts/services/LanguageService.java
@@ -0,0 +1,8 @@
+package com.springsakila.inventory.domain.contracts.services;
+
+
+import com.springsakila.inventory.domain.entities.Language;
+
+public interface LanguageService extends ProjectionDomainService<Language, Integer> {
+
+}
