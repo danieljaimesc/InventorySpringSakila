30c554bffd98884249f0d1a6203a8aaffc3644ed
Daniel Jaimes Carrillo <daniel.jaimes-carrillo@capgemini.com>
2023-03-30 11:49:21 +0200

    domain contracts services

---
+++ src/main/java/com/springsakila/inventory/domain/contracts/services/ActorService.java
@@ -0,0 +1,8 @@
+package com.springsakila.inventory.domain.contracts.services;
+
+
+import com.springsakila.inventory.domain.entities.Actor;
+
+public interface ActorService extends ProjectionDomainService<Actor, Integer> {
+
+}
