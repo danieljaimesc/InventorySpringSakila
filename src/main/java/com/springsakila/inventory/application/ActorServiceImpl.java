2c166d19c5427c5d22eecfd23964493baba256eb
Daniel Jaimes Carrillo <daniel.jaimes-carrillo@capgemini.com>
2023-03-31 12:25:11 +0200

    moved

--- src/main/java/com/springsakila/inventory/application/ActorServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.springsakila.inventory.application;
-
-import com.springsakila.inventory.domain.contracts.services.ActorService;
-
-public class ActorServiceImpl implements ActorService {
-}
